4b13c91600a7ac6e85f19df1acfe84ca3ffd2267
egraur <efim.graur@ellation.com>
2021-08-04 23:49:52 +0300

    improve button groups

--- src/components/ButtonGroup/BasicButtonGroup.js
+++ src/components/ButtonGroup/BasicButtonGroup.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { makeStyles } from "@material-ui/core/styles";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    '& > *': {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& > *": {
       margin: theme.spacing(1),
     },
   },
@@ -19,10 +21,17 @@ export default function BasicButtonGroup() {
 
   return (
     <div className={classes.root}>
-      <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
+      <ButtonGroup
+        variant="contained"
+        color="primary"
+        aria-label="contained primary button group"
+      >
+        <Button>
+          <EditIcon />
+        </Button>
+        <Button>
+          <DeleteForeverIcon />
+        </Button>
       </ButtonGroup>
     </div>
   );
